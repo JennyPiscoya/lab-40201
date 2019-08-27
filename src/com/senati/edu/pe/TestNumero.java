73237da6c876a256c4afea65e8c3af9b6c14362c
Alumno <Alumno@192.168.106.58>
2019-08-27 12:43:05 -0500

    Tengo hambre

---
+++ src/com/senati/edu/pe/TestNumero.java
@@ -0,0 +1,22 @@
+package com.senati.edu.pe;
+import java.util.Scanner;
+public class TestNumero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {    
+			Numero Onum = new Numero();    
+			Scanner Leer = new Scanner(System.in);    
+			System.out.print("\n Ingrese el número : ");    
+			double n = Leer.nextDouble();    
+			Onum.Num = n;   
+			
+			System.out.println(" " + Onum.Primo());    
+			System.out.println(" " + Onum.Perfecto());    
+			System.out.println(" Factorial " + Onum.Factorial());   
+		}catch (Exception ex) {       
+			
+		} 
+	}
+
+}
